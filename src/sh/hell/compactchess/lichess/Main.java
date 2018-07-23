10a98ead026b8424f925ea519acb29bafb481ff4
Tim Speckhals <github@timmyrs.de>
2018-07-23 19:09:06 +0200

    Some pre-release files

---
+++ src/sh/hell/compactchess/lichess/Main.java
@@ -0,0 +1,9 @@
+package sh.hell.compactchess.lichess;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("CompactChessLichess <https://hell.sh/CompactChess/lichess> is an extension for CompactChess <https://hell.sh/CompactChess> which allows you to interact with the Lichess <https://lichess.org> API.");
+	}
+}
