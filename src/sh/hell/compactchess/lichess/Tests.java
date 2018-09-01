4f82ac83fb298ea5181958f4bdb77881a855d0f7
Tim Speckhals <github@timmyrs.de>
2018-09-01 16:27:03 +0200

    Update Unit Tests

--- src/sh/hell/compactchess/lichess/Tests.java
+++ src/sh/hell/compactchess/lichess/Tests.java
@@ -9,6 +9,8 @@ import sh.hell.compactchess.game.GameStatus;
 import java.io.IOException;
 
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
+import static junit.framework.TestCase.assertTrue;
 
 public class Tests
 {
@@ -24,7 +26,8 @@ public class Tests
 		final LichessAPI lichessAPI = new LichessAPI();
 		final Game game = Game.fromPGN("[Event \"Grandmasters-Young Masters\"]\n" + "[Site \"Sochi URS\"]\n" + "[Date \"1970.10.??\"]\n" + "[EventDate \"?\"]\n" + "[Round \"9\"]\n" + "[Result \"1-0\"]\n" + "[White \"Viktor Kupreichik\"]\n" + "[Black \"Mikhail Tal\"]\n" + "[ECO \"B57\"]\n" + "[WhiteElo \"?\"]\n" + "[BlackElo \"?\"]\n" + "[PlyCount \"61\"]\n" + "\n" + "1.e4 c5 2.Nf3 d6 3.d4 cxd4 4.Nxd4 Nc6 5.Nc3 Nf6 6.Bc4 Qb6\n" + "7.Nb3 e6 8.Be3 Qc7 9.f4 a6 10.Bd3 b5 11.a3 Be7 12.Qf3 Bb7\n" + "13.O-O Rc8 14.Rae1 O-O 15.Qh3 b4 16.Nd5 exd5 17.exd5 Nb8\n" + "18.Bd4 g6 19.Rf3 Bxd5 20.Rfe3 Bd8 21.Qh4 Nbd7 22.Qh6 Qb7\n" + "23.Rg3 Nc5 24.Nxc5 dxc5 25.f5 cxd4 26.fxg6 fxg6 27.Bxg6 Kh8\n" + "28.Qxf8+ Ng8 29.Bf5 Rb8 30.Re8 Qf7 31.Rh3 1-0").get(0);
 		String url = lichessAPI.importGame(game);
-		assertEquals("https://lichess.org/3jwajJiC", url);
+		assertNotNull(url);
+		assertTrue(url.startsWith("https://lichess.org/"));
 	}
 
 	@Test(timeout = 10000L)
