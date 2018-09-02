1033a8991a0791b2f43610d575ad8deb5218e73f
Tim Speckhals <github@timmyrs.de>
2018-09-02 15:12:07 +0200

    Update Main.java

--- src/sh/hell/compactchess/lichess/Main.java
+++ src/sh/hell/compactchess/lichess/Main.java
@@ -4,6 +4,6 @@ public class Main
 {
 	public static void main(String[] args)
 	{
-		System.out.println("CompactChessLichess <https://hell.sh/CompactChess/lichess> is an extension for CompactChess <https://hell.sh/CompactChess> which allows you to interact with the Lichess <https://lichess.org> API.");
+		System.out.println("CompactLichess <https://hell.sh/CompactChess/lichess> is an extension for CompactChess <https://hell.sh/CompactChess> which allows you to interact with the Lichess <https://lichess.org> API.");
 	}
 }
